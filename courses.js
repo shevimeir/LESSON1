46295c1750acd98fa53d6d4ba3b4c7310755bd4d
shevimeir <shm8506744@gmail.com>
2026-07-03 00:31:38 +0300

    Delete courses.js

--- courses.js
+++
@@ -1,7 +0,0 @@
-const courses = [
-    { id: 101, name: "HTML & CSS Basics", duration: "8 weeks" },
-    { id: 102, name: "JavaScript Advanced", duration: "10 weeks" },
-    { id: 103, name: "Node.js Backend", duration: "12 weeks" }
-];
-
-module.exports = courses;
